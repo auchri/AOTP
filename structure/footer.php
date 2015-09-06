fd522ed69253cc27020e9b70d485cbeeb7b1a610
Christoph Auer <auer.chrisi@gmx.net>
2015-09-07 01:55:13 +0200

    Fixed footer padding

--- structure/footer.php
+++ structure/footer.php
@@ -1,10 +1,12 @@
 </div>
-<div id="footer">
-    GitHub: <a href="https://github.com/auchri">auchri</a>/<a href="https://github.com/auchri/AOTP" target="_blank">AOTP</a> -
-    <a class="github-button" href="https://github.com/auchri/AOTP" data-icon="octicon-star" data-count-href="/auchri/AOTP/stargazers"
-       data-count-api="/repos/auchri/AOTP#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star auchri/AOTP on GitHub">Star</a>
-    - Licensed under <a
-        href="<?= URI_ROOT ?>LICENSE">GPL v3</a>
+<div id="footerContainer">
+    <div id="footer">
+        GitHub: <a href="https://github.com/auchri">auchri</a>/<a href="https://github.com/auchri/AOTP" target="_blank">AOTP</a> -
+        <a class="github-button" href="https://github.com/auchri/AOTP" data-icon="octicon-star" data-count-href="/auchri/AOTP/stargazers"
+           data-count-api="/repos/auchri/AOTP#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star auchri/AOTP on GitHub">Star</a>
+        - Licensed under <a
+            href="<?= URI_ROOT ?>LICENSE">GPL v3</a>
+    </div>
 </div>
 </div>
 </div>
