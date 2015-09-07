2bc8d879f0c8a1dc6ba63e35a2320a015be15e9b
Christoph Auer <auer.chrisi@gmx.net>
2015-09-07 13:03:51 +0200

    Add copyright to footer

--- structure/footer.php
+++ structure/footer.php
@@ -1,6 +1,7 @@
 </div>
 <div id="footerContainer">
     <div id="footer">
+        © <?= date('o') ?> <a target="_blank" href="http://www.auerchri.at">Christoph Auer</a> -
         GitHub: <a href="https://github.com/auchri">auchri</a>/<a href="https://github.com/auchri/AOTP" target="_blank">AOTP</a> -
         <a class="github-button" href="https://github.com/auchri/AOTP" data-icon="octicon-star" data-count-href="/auchri/AOTP/stargazers"
            data-count-api="/repos/auchri/AOTP#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star auchri/AOTP on GitHub">Star</a>
