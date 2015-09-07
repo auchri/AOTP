4d218cf83cc32523532383c2af365f14878daae0
Christoph Auer <auer.chrisi@gmx.net>
2015-09-07 13:13:02 +0200

    Fix statistics

--- statistic.php
+++ statistic.php
@@ -1,28 +1,33 @@
 <?php
 
-include 'main.php';
+use AOTP\Config;
 
-$langPath = $langDir . $defaultLang . DIRECTORY_SEPARATOR;
+include 'config/main.php';
 
-$defaultLangFile = isset($_GET['file']) ? $langPath . trim($_GET['file']) : '';
+$defaultLangFile = isset($_GET['file']) ? $config->getDefaultLanguageDirectory() . trim($_GET['file']) : '';
 
 if (empty($defaultLangFile) || !file_exists($defaultLangFile)) {
     header('Location: /');
     exit();
 }
 
+$_GET['file'] = trim($_GET['file']);
+
+$structure->includeHeader($_GET['file']);
+
 function getNumberOfStrings($file) {
     $content = file_get_contents($file);
-    preg_match_all("/<string name=\"([^\"]+)\">(.*)<\/string>/", $content, $output_array);
+    preg_match_all("/<string name=\"([^\"]+)\">(.*)<\\/string>/", $content, $output_array);
 
     return count($output_array[1]);
 }
 
 $standardLangStrings = getNumberOfStrings($defaultLangFile);
 
+$toTranslate = $config->getValue(Config::KEY_TO_TRANSLATE);
 $langStrings = array();
 foreach ($toTranslate as $language => $longName) {
-    $translateDir  = $langDir . $language . DIRECTORY_SEPARATOR;
+    $translateDir  = DIR_LANGUAGES . $language . DIRECTORY_SEPARATOR;
     $translateFile = $translateDir . $_GET['file'];
     $strings       = 0;
 
@@ -36,35 +41,29 @@ foreach ($toTranslate as $language => $longName) {
 }
 arsort($langStrings);
 ?>
-<style type="text/css">
-    .ok { color: #009900; }
+    <h1>File <?= $_GET['file'] ?></h1>
+    Strings to translate: <?= $standardLangStrings ?>
+    <h2>Status</h2>
+    <ul>
+        <?php
+        foreach ($langStrings as $language => $strings) {
+            $percent = $strings / $standardLangStrings * 100;
+            if ($percent < 30) {
+                $class = 'error';
+            } else if ($percent < 100) {
+                $class = 'warning';
+            } else {
+                $class = 'ok';
+            }
 
-    .warning { color: #FF9900; }
+            $name = $toTranslate[$language] . ' (' . $language . ')';
 
-    .error { color: #CC0000; }
-
-    a { color: inherit; }
-</style>
-<h1>File <?= $_GET['file'] ?></h1>
-Strings to translate: <?= $standardLangStrings ?>
-<h2>Status</h2>
-<ul>
-    <?php
-    foreach ($langStrings as $language => $strings) {
-        $percent = $strings / $standardLangStrings * 100;
-        if ($percent < 30) {
-            $class = 'error';
-        } else if ($percent < 100) {
-            $class = 'warning';
-        } else {
-            $class = 'ok';
+            echo '<li class="' . $class . '"><a class="' . $class . '" href="strings.php?file=' . $_GET['file'] . '&amp;language=' . $language . '">' . $name . ': ' . $strings . ' (' . $percent . '%)</a></li>';
         }
+        ?>
+    </ul>
 
-        $name = $toTranslate[$language] . ' (' . $language . ')';
+    <a href="/">Zurück</a>
 
-        echo '<li class="' . $class . '"><a href="strings.php?file=' . $_GET['file'] . '&amp;language=' . $language . '">' . $name . ': ' . $strings . ' (' . $percent . '%)</a></li>';
-    }
-    ?>
-</ul>
-
-<a href="/">Zurück</a>
+<?php
+$structure->includeFooter();
