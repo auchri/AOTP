db9a141b939cfcd83fb533f23157a96b1cd67ce0
Christoph Auer <auer.chrisi@gmx.net>
2015-09-07 22:43:34 +0200

    Split database connection in two methods

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -13,15 +13,30 @@ class Database
      */
     public static function getConnection() {
         if (self::$connection == null) {
-            $dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST;
-
-            $db = null;
-            $db = new \PDO($dsn, DB_USER, DB_PASSWORD, array(\PDO::ATTR_PERSISTENT => true, \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-            $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-            $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
-            self::$connection = $db;
+            $pdo              = self::connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+            self::$connection = $pdo;
         }
 
         return self::$connection;
+    }
+
+    /**
+     * Connects to the database with the give parameters
+     *
+     * @param $host     string
+     * @param $username string
+     * @param $password string
+     * @param $database string
+     *
+     * @return \PDO
+     */
+    public static function connect($host, $username, $password, $database) {
+        $dsn = 'mysql:dbname=' . $database . ';host=' . $host;
+
+        $pdo = @new \PDO($dsn, $username, $password, array(\PDO::ATTR_PERSISTENT => true, \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+
+        return $pdo;
     }
 }
