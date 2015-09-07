b34aca997d06cc7cdffaf3aecff5d37926f4e362
Christoph Auer <auer.chrisi@gmx.net>
2015-09-07 14:24:49 +0200

    Fix format

--- structure/footer.php
+++ structure/footer.php
@@ -5,8 +5,7 @@
         GitHub: <a href="https://github.com/auchri">auchri</a>/<a href="https://github.com/auchri/AOTP" target="_blank">AOTP</a> -
         <a class="github-button" href="https://github.com/auchri/AOTP" data-icon="octicon-star" data-count-href="/auchri/AOTP/stargazers"
            data-count-api="/repos/auchri/AOTP#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star auchri/AOTP on GitHub">Star</a>
-        - Licensed under <a
-            href="<?= URI_ROOT ?>LICENSE">GPL v2</a>
+        - Licensed under <a href="<?= URI_ROOT ?>LICENSE">GPL v2</a>
     </div>
 </div>
 </div>
