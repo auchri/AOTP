4e1e72490b82c1635c1f9a5351ab7e8fe44f0c12
Christoph Auer <auer.chrisi@gmx.net>
2015-09-08 18:34:28 +0200

    Add methods to Filesystem

--- classes/Filesystem.class.php
+++ classes/Filesystem.class.php
@@ -2,6 +2,8 @@
 
 namespace AOTP;
 
+use DirectoryIterator;
+
 /**
  * Class for filesystem methods
  *
@@ -18,5 +20,42 @@ class Filesystem
      */
     public static function fileExists($path) {
         return file_exists($path) && is_file($path);
+    }
+
+    /**
+     * Checks if the given directory exists
+     *
+     * @param  string $path
+     *
+     * @return bool
+     */
+    public static function dirExists($path) {
+        return file_exists($path) && is_dir($path);
+    }
+
+    /**
+     * Deletes all the contents of a directory
+     *
+     * @param string     $dir
+     * @param bool|false $deleteRoot
+     */
+    public static function deleteRecursive($dir, $deleteRoot = false) {
+        if (!self::dirExists($dir)) {
+            return;
+        }
+
+        foreach (new DirectoryIterator($dir) as $file) {
+            if ($file->isDot()) continue;
+
+            if ($file->isFile()) {
+                unlink($file->getPathname());
+            } elseif ($file->isDir()) {
+                self::deleteRecursive($file->getPathname() . DIRECTORY_SEPARATOR, true);
+            }
+        }
+
+        if ($deleteRoot) {
+            rmdir($dir);
+        }
     }
 }
