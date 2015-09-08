c422979094ebef3b876ec8e592f8ce71e9fa0ac4
Christoph Auer <auer.chrisi@gmx.net>
2015-09-08 14:31:14 +0200

    Update config template

--- config/templates/user.php
+++ config/templates/user.php
@@ -1,6 +1,6 @@
 <?php
-// DATABASE
-define('DB_HOST', '{db_host}');
-define('DB_USER', '{db_user}');
-define('DB_PASSWORD', '{db_password}');
-define('DB_NAME', '{db_name}');
+####### DATABASE ##########################################
+define('DB_HOST', '{db_host}'); // Hostname of the database server
+define('DB_USER', '{db_user}'); // Database user
+define('DB_PASSWORD', '{db_password}'); // Database password
+define('DB_NAME', '{db_name}'); // Name of the database
