f9e34443b655a4b4196a0e80df8b3050cd8a083a
Christoph Auer <auer.chrisi@gmx.net>
2015-09-09 14:04:58 +0200

    Improve mobile design

--- structure/header.php
+++ structure/header.php
@@ -4,6 +4,7 @@
     <title><?= $title ?></title>
     <meta charset="UTF-8"/>
     <meta name="description" content="AOTP"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
     <link href="<?= URI_CSS ?>reset.css" type="text/css" rel="stylesheet"/>
     <link href="<?= URI_CSS ?>basic.css" type="text/css" rel="stylesheet"/>
