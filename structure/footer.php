1f10754c7fb4a403982819b2be6e7eda90441005
Christoph Auer <auer.chrisi@gmx.net>
2015-09-09 16:13:14 +0200

    Use placeholders in templates, add style for menu

--- structure/footer.php
+++ structure/footer.php
@@ -1,16 +1,16 @@
 </div>
 <div id="footerContainer">
     <div id="footer">
-        © <?= date('o') ?> <a target="_blank" href="http://www.auerchri.at">Christoph Auer</a> -
+        © [[YEAR]] <a target="_blank" href="http://www.auerchri.at">Christoph Auer</a> -
         GitHub: <a href="https://github.com/auchri">auchri</a>/<a href="https://github.com/auchri/AOTP" target="_blank">AOTP</a> -
         <a class="github-button" href="https://github.com/auchri/AOTP" data-icon="octicon-star" data-count-href="/auchri/AOTP/stargazers"
            data-count-api="/repos/auchri/AOTP#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star auchri/AOTP on GitHub">Star</a>
-        - Licensed under <a href="<?= URI_ROOT ?>LICENSE">GPL v2</a>
+        - Licensed under <a href="[[URI_ROOT]]LICENSE">GPL v2</a>
     </div>
 </div>
 </div>
 </div>
-<!-- Javascript's -->
+<!-- Javascript -->
 <script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script>
 </body>
 </html>
--- structure/header.php
+++ structure/header.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title><?= $title ?></title>
+    <title>[[TITLE]]</title>
     <meta charset="UTF-8"/>
     <meta name="description" content="AOTP"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-    <link href="<?= URI_CSS ?>reset.css" type="text/css" rel="stylesheet"/>
-    <link href="<?= URI_CSS ?>basic.css" type="text/css" rel="stylesheet"/>
-    <link href="<?= URI_CSS ?>structure.css" type="text/css" rel="stylesheet"/>
+    <link href="[[URI_CSS]]reset.css" type="text/css" rel="stylesheet"/>
+    <link href="[[URI_CSS]]basic.css" type="text/css" rel="stylesheet"/>
+    <link href="[[URI_CSS]]structure.css" type="text/css" rel="stylesheet"/>
 </head>
 <body>
     <div id="headerContainer">
         <div class="centeredContainer">
-            <div id="header"><h1><a href="<?= URI_ROOT ?>">Android Open Translation Platform</a></h1></div>
+            <header id="header">
+                <h1 title="Android Open Translation Platform"><a href="[[URI_ROOT]]">AOTP</a></h1>
+                <ul>[[MENU_ITEMS]]</ul>
+            </header>
         </div>
     </div>
     <div class="centeredContainer">
