5dba6eca31aba90edb6da6ae6ea63caedc93538d
Christoph Auer <auer.chrisi@gmx.net>
2015-09-09 16:14:09 +0200

    Replace placeholders, delete obsolete class

--- classes/FrontController.class.php
+++ classes/FrontController.class.php
@@ -12,6 +12,14 @@ class FrontController extends Singleton
     private $title;
     private $content;
 
+    private $menuEntries = array();
+
+    protected function __construct() {
+        $this->menuEntries = array(
+            'Projects' => Uri::getAOTPModuleUri('Projects')
+        );
+    }
+
     public function setTitle($title) {
         $this->title = $title;
     }
@@ -21,11 +29,32 @@ class FrontController extends Singleton
     }
 
     public function printPage() {
-        Structure::getInstance()->includeHeader($this->title);
+        echo $this->getHeaderHtml();
         echo $this->getContent();
-        Structure::getInstance()->includeFooter();
+        echo $this->getFooterHtml();
     }
 
+    /**
+     * Returns the title of the site
+     *
+     * @return mixed
+     */
+    public function getTitle() {
+        if (is_string($this->title)) {
+            $title = array($this->title);
+        }
+
+        $title[] = 'AOTP';
+        $title   = implode(' | ', $title);
+
+        return $title;
+    }
+
+    /**
+     * Returns the content of the site
+     *
+     * @return mixed
+     */
     public function getContent() {
         return $this->content;
     }
@@ -45,5 +74,58 @@ class FrontController extends Singleton
 
         $this->setTitle($site->getTitle());
         $this->setContent($output);
+    }
+
+    /**
+     * Returns the html of the menu items
+     *
+     * @return string
+     */
+    private function getMenuItemHtml() {
+        $html = '';
+
+        foreach ($this->menuEntries as $title => $url) {
+            $html .= '<li><a href="' . $url . '">' . $title . '</a></li>';
+        }
+
+        return $html;
+    }
+
+    /**
+     * Returns the html of the header
+     *
+     * @return string
+     */
+    private function getHeaderHtml() {
+        $placeholderReplacements = array(
+            '[[TITLE]]'      => $this->getTitle(),
+            '[[URI_ROOT]]'   => URI_ROOT,
+            '[[URI_CSS]]'    => URI_CSS,
+            '[[MENU_ITEMS]]' => $this->getMenuItemHtml()
+        );
+        $placeholder             = array_keys($placeholderReplacements);
+        $replacement             = array_values($placeholderReplacements);
+
+        $headerHtml = file_get_contents(DIR_STRUCTURE . 'header.php');
+
+        return str_replace($placeholder, $replacement, $headerHtml);
+    }
+
+    /**
+     * Returns the html of the footer
+     *
+     * @return string
+     */
+    private function getFooterHtml() {
+        $placeholderReplacements = array(
+            '[[URI_ROOT]]' => URI_ROOT,
+            '[[YEAR]]'     => date('o')
+        );
+        $placeholder             = array_keys($placeholderReplacements);
+        $replacement             = array_values($placeholderReplacements);
+
+        $headerHtml = file_get_contents(DIR_STRUCTURE . 'footer.php');
+
+        return str_replace($placeholder, $replacement, $headerHtml);
     }
 }
